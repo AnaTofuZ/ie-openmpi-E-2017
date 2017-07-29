dab881919678f598ad2733e8c2dfbe429b63d925
AnaTofuZ <anatofuz@gmail.com>
2017-07-29 17:31:44 +0900

    main関数の実装

--- pi_pthread.c
+++ pi_pthread.c
@@ -3,14 +3,27 @@
 #include <math.h>
 #include <pthread.h>
 
+#define THREAD 10
+#define ITERATION 1000
+
 void calc_pi(void *arg);
 
 int main(void){
 
     time_t start,end;
+    int targ[THREAD];
+    pthread_t handle[THREAD];
 
     start = time(NULL);
 
+    for (int  i = 0; i < THREAD; i++) {
+        targ[i]=i;
+        pthread_create(&handle[i],NULL,(void*)calc_pi,(void*)&targ[i]); //thread作成
+    }
+
+    for (int i = 0; i < THREAD; i++) {
+        pthread_join(handle[i],NULL);
+    }
 
     end = time(NULL);
     printf("%ld sec\n",end - start);
@@ -22,4 +35,8 @@ int main(void){
 // TODO 実際の処理
 
 void calc_pi(void *arg){
+
+    int* targ = (int *)arg;
+
+    int c_start,c_end,rane,limit;
 }
