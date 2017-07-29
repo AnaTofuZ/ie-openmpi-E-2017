17e2a5230ce71661aa7948c0a9db88445b07c172
KaitoIshizuka <e155716@ie.u-rukyu.ac.jp>
2017-07-29 14:14:14 +0700

    めちゃ遅い

--- margesort_parallel.c
+++ margesort_parallel.c
@@ -20,7 +20,7 @@ static unsigned long count3=0;
   /* 配列 x[ ] の left から right の要素のマージソートを行う */
 void MergeSort(long x[ ], long left, long right)
 {
-    long mid,  j, k;
+    long mid, i, j, k;
 	
 
 
@@ -32,36 +32,55 @@ void MergeSort(long x[ ], long left, long right)
     mid = (left + right) / 2;       /* 中央の値より */
 
 //	printf("mid=%d\n",mid);
-	  MergeSort(x, left, mid);        /* 左を再帰呼び出し */
-	  MergeSort(x, mid + 1, right);   /* 右を再帰呼び出し */
 
-	#pragma omp sections
+/*	#pragma omp parallel num_threads(10)
 	{
-	#pragma omp section
+	  #pragma omp sections
+	  {
+	  #pragma omp section
+	  MergeSort(x, left, mid);        // 左を再帰呼び出し
+	  #pragma omp section
+	  MergeSort(x, mid + 1, right);   // 右を再帰呼び出し
+	  }
+	}
+	*/
+
+	  MergeSort(x, left, mid);        // 左を再帰呼び出し
+	  MergeSort(x, mid + 1, right);   // 右を再帰呼び出し
+
+
+
+
+	#pragma omp parallel num_threads(2) private(i,j),shared(temp,x,left,right,mid)
+	{
+	#pragma omp sections 
 	{
       /* x[left] から x[mid] を作業領域にコピー */
-	#pragma omp parallel for schedule(guided)
-    for (long i  = left; i <= mid; i++)
+	#pragma omp section
+	{
+    for (i  = left; i <= mid; i++)
         temp[i] = x[i];
 	}
 
 
+      /* x[mid + 1] から x[right] は逆順にコピー */
 	#pragma omp section
 	{
-      /* x[mid + 1] から x[right] は逆順にコピー */
 	j = right;
-	#pragma omp parallel for schedule(guided)
-    for (long i = mid + 1; i <= right; i++)
+    for (i = mid + 1; i <= right; i++)
 	{
         temp[i] = x[j];
-	}
+		j--;
 	}
 	}
 
-    long i = left;         /* i とj は作業領域のデーターを */
+	}
+	}
+
+    i = left;         /* i とj は作業領域のデーターを */
     j = right;        /* k は配列の要素を指している */
 
-	#pragma omp parallel for schedule(guided)
+
     for (k = left; k <= right; k++){    /* 小さい方から配列に戻す */
         if (temp[i] <= temp[j])        /* ここでソートされる */
             x[k] = temp[i++];
@@ -78,6 +97,7 @@ int main(void)
 {
 
 	static long x[MAX_DATA] = {0};
+	static long y[10] = {3,6,8,9,0,2,1,4,5,7};
 	fprintf(stderr, "exe1\n");
     int i;
     clock_t start,end;
@@ -97,9 +117,29 @@ int main(void)
 
 */
 
+
+
+      /* ソート前のデータを表示 */
+    printf("ソート前\n");
+    for (i = 0; i < 10; i++)
+        printf("%d\t", y[i]);
+
+
+    start = clock();
+	MergeSort(y, (long)0, (long)10 - 1);
+    end = clock();
+    printf("%.2f秒かかりました\n",(double)(end-start)/CLOCKS_PER_SEC);
+
+      /* ソート後のデータを表示 */
+    fprintf(stderr,"ソート後\n");
+    for (i = 0; i < 10; i++)
+        fprintf(stderr,"%d\t", y[i]);
+
+
     start = clock();
 	MergeSort(x, (long)0, (long)MAX_DATA - 1);
     end = clock();
+    printf("%.2f秒かかりました\n",(double)(end-start)/CLOCKS_PER_SEC);
 
       /* ソート後のデータを表示 */
 /*    printf("ソート後\n");
@@ -107,7 +147,6 @@ int main(void)
         printf("%d\t", x[i]);
 */
 
-    printf("%.2f秒かかりました\n",(double)(end-start)/CLOCKS_PER_SEC);
 	return 0;
 }
 
